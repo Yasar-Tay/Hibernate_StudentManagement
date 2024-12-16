74650d28e55a1317515154b4813c987b421100aa
Yasar TAY <yasartay@gmail.com>
2024-12-16 22:25:20 +0100

    Rolledback changes in StudentRepository class

--- src/main/java/StudentRepository.java
+++ src/main/java/StudentRepository.java
@@ -2,30 +2,4 @@ import java.sql.*;
 
 public class StudentRepository {
     //Repository class
-    Connection connection;
-    Statement statement;
-
-    {
-        try {
-            connection = DriverManager.getConnection("hibernate.cfg.xml");
-            statement = connection.createStatement();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void registerStudent(Student student) throws SQLException {
-
-        String hql = "INSERT INTO Student(id, name, grade) VALUES (?, ?, ?)";
-
-        PreparedStatement preparedStatement = connection.prepareStatement(hql);
-        preparedStatement.setInt(1, student.getId());
-        preparedStatement.setString(2, student.getName());
-        preparedStatement.setInt(3, student.getGrade());
-
-        System.out.println(student);
-    }
-
-
-
 }
